7c306d1595c6c01c934d5e581fc39112d22ffa71
nitesh-abjima <128471408+nitesh-abjima@users.noreply.github.com>
2023-04-28 18:55:52 +0530

    Assignment-2

--- ASPWebAPI/ASPWebAPI/Program.cs
+++ ASPWebAPI/ASPWebAPI/Program.cs
@@ -1,8 +1,16 @@
+using ASPWebAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+builder.Services.AddDbContext<StudentsDataContext>(options =>
+options.UseSqlServer(builder.Configuration.GetConnectionString("dbconn")));
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
