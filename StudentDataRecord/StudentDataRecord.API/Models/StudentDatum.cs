739f99d0f7397b3058e1c2eba73e764b5eef3fc0
Bhawani Singh <bhawani.singh@abjima.com>
2023-04-28 18:29:47 +0530

    Assigment-2-Completed

---
+++ StudentDataRecord/StudentDataRecord.API/Models/StudentDatum.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace StudentDataRecord.API.Models
+{
+    public partial class StudentDatum
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+        public int RollNo { get; set; }
+        public string FamilyName { get; set; } = null!;
+        public string Address { get; set; } = null!;
+        public int Contact { get; set; }
+    }
+}
