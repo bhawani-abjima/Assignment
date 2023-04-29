65e31ddcfb75c673e541b967ccaaa27db5a4ad89
nitesh-abjima <128471408+nitesh-abjima@users.noreply.github.com>
2023-04-29 18:50:57 +0530

    EF CFA

---
+++ WebApiEFCFA/Data/WebApiEFCFAContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WebApiEFCFA.Model;
+
+namespace WebApiEFCFA.Data
+{
+    public class WebApiEFCFAContext : DbContext
+    {
+        public WebApiEFCFAContext (DbContextOptions<WebApiEFCFAContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<WebApiEFCFA.Model.Employee> Employee { get; set; } = default!;
+    }
+}
---
+++ WebApiEFCFA/Model/Employee.cs
@@ -0,0 +1,11 @@
+﻿namespace WebApiEFCFA.Model
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string? employeename { get; set; }
+        public string? Designation { get; set; }
+        public int contact { get; set; }
+
+    }
+}
