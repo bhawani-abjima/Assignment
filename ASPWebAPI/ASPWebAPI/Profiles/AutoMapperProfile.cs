47530e7a953eecfdfde3be225abeac8bf8ff74d5
Bhawani Singh <bhawani.singh@abjima.com>
2023-05-06 18:36:02 +0530

    Initial Commit

---
+++ ASPWebAPI/ASPWebAPI/Profiles/AutoMapperProfile.cs
@@ -0,0 +1,14 @@
+﻿using ASPWebAPI.DTO;
+using ASPWebAPI.Models;
+using AutoMapper;
+
+namespace ASPWebAPI.Profiles
+{
+    public class AutoMapperProfile : Profile
+    {
+        public AutoMapperProfile()
+        {
+            CreateMap<Student, StudentDTO>();
+        }
+    }
+}
---
+++ ASPWebAPI/ASPWebAPI/Program.cs
@@ -0,0 +1,37 @@
+using ASPWebAPI.Models;
+using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+
+builder.Services.AddDbContext<StudentsDataContext>(options =>
+options.UseSqlServer(builder.Configuration.GetConnectionString("dbconn")));
+
+builder.Services.AddAutoMapper(typeof(Program).Assembly);
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
