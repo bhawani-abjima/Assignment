b6c392a5d681e87dd305aa80e92bf1633fdb2c76
nitesh-abjima <128471408+nitesh-abjima@users.noreply.github.com>
2023-05-08 18:32:53 +0530

    Add DI & Exception handling

---
+++ ASPWebAPI/ASPWebAPI/Repository/StudentRepo.cs
@@ -0,0 +1,25 @@
+﻿using ASPWebAPI.Infrastructure;
+using ASPWebAPI.Models;
+
+namespace ASPWebAPI.Repository
+{
+    public class StudentRepo : IStudentRepo
+    {
+        private readonly StudentsDataContext _context;
+
+        public StudentRepo(StudentsDataContext context)
+        {
+            _context = context;
+        }
+
+        public List<Student> GetAll()
+        {
+            return _context.Students.ToList();
+        }
+
+        public Student GetByID(int id)
+        {
+            return _context.Students.FirstOrDefault(x => x.Id == id);
+        }
+    }
+}
