c1fcbe42fe15fb12e9ce2d5d5ec1ab2537292c45
Bhawani Singh <bhawani.singh@abjima.com>
2023-05-10 19:01:25 +0530

    New Commit

---
+++ StudentsApp/Models/Student.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace StudentsApp.Models
+{
+    public partial class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+        public int RollNo { get; set; }
+        public string FamilyName { get; set; } = null!;
+        public string Address { get; set; } = null!;
+        public int Contact { get; set; }
+    }
+}
