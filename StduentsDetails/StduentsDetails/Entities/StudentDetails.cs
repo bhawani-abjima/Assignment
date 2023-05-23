d1adb3ec1e53ad5c12512f46c32acce0ad5d431b
Bhawani Singh <bhawani.singh@abjima.com>
2023-05-23 18:06:42 +0530

    Update in validation

--- StduentsDetails/StduentsDetails/Entities/StudentDetails.cs
+++ StduentsDetails/StduentsDetails/Entities/StudentDetails.cs
@@ -1,4 +1,7 @@
-﻿namespace StudentsDetails.Entities
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.RegularExpressions; 
+
+namespace StudentsDetails.Entities
 
 {
     public class StudentDetails
@@ -8,6 +11,10 @@
         public int RollNo { get; set; }
         public string FamilyName { get; set; } = null!;
         public string Address { get; set; } = null!;
+
+        [Display(Name = "Mobile Number:")]
+        [Required(ErrorMessage = "Mobile Number is required.")]
+        [RegularExpression("^([0-9]{10})$", ErrorMessage = "Invalid Mobile Number.")]
         public long? Contact { get; set; }
     }
 }
--- StduentsDetails/StduentsDetails/Models/StudentDatum.cs
+++ StduentsDetails/StduentsDetails/Models/StudentDatum.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text.RegularExpressions;
 
 namespace StudentsDetails.Models
 {
@@ -10,6 +12,10 @@ namespace StudentsDetails.Models
         public int RollNo { get; set; }
         public string FamilyName { get; set; } = null!;
         public string Address { get; set; } = null!;
+
+        [Display(Name = "Contact No")]
+        [Required(ErrorMessage = "Mobile Number is required.")]
+        [RegularExpression("^([0-9]{10})$", ErrorMessage = "Invalid Mobile Number.")]
         public long? Contact { get; set; }
     }
 }
